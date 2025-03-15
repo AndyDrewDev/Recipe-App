25d828dbb8a4d04119b3dcc09e24b5823b2cae0c
AndyDrewDev <andydrew.stamp@gmail.com>
2025-03-15 22:08:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,211 @@
+const mealsElement = document.getElementById('meals')
+const favoriteContainer = document.getElementById('fav-meals')
+const mealPopup = document.getElementById('meal-popup')
+const mealInfoElement = document.getElementById('meal-info')
+const popupCloseBtn = document.getElementById('close-popup')
+
+const searchTerm = document.getElementById('search-term')
+const searchBtn = document.getElementById('search')
+
+getRandomMeal()
+fetchFavorites()
+
+async function getRandomMeal() {
+  const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  const respData = await resp.json()
+  const randomMeal = respData.meals[0]
+
+  addMeal(randomMeal, true)
+}
+
+async function getMealById(id) {
+  const resp = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id
+  )
+
+  const respData = await resp.json()
+  const meal = respData.meals[0]
+
+  return meal
+}
+
+async function getMealsBySearch(term) {
+  const resp = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=' + term
+  )
+
+  const respData = await resp.json()
+  const meals = respData.meals
+
+  return meals
+}
+
+function addMeal(mealData, random = false) {
+  console.log(mealData)
+
+  const meal = document.createElement('div')
+  meal.classList.add('meal')
+
+  meal.innerHTML = `
+        <div class="meal-header">
+            ${
+              random
+                ? `
+            <span class="random"> Random Recipe </span>`
+                : ''
+            }
+            <img
+                src="${mealData.strMealThumb}"
+                alt="${mealData.strMeal}"
+            />
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <i class="fas fa-heart"></i>
+            </button>
+        </div>
+    `
+
+  const btn = meal.querySelector('.meal-body .fav-btn')
+
+  btn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    if (btn.classList.contains('active')) {
+      removeMealFromLocalStorage(mealData.idMeal)
+      btn.classList.remove('active')
+    } else {
+      addMealToLocalStorage(mealData.idMeal)
+      btn.classList.add('active')
+    }
+
+    fetchFavorites()
+  })
+
+  meal.addEventListener('click', () => {
+    showMealInfo(mealData)
+  })
+
+  mealsElement.appendChild(meal)
+}
+
+function addMealToLocalStorage(mealId) {
+  const mealIds = getMealsFromLocalStorage()
+
+  localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealId]))
+}
+
+function removeMealFromLocalStorage(mealId) {
+  const mealIds = getMealsFromLocalStorage()
+
+  localStorage.setItem(
+    'mealIds',
+    JSON.stringify(mealIds.filter((id) => id !== mealId))
+  )
+}
+
+function getMealsFromLocalStorage() {
+  const mealIds = JSON.parse(localStorage.getItem('mealIds'))
+
+  return mealIds || []
+}
+
+async function fetchFavorites() {
+  // clean the container
+  favoriteContainer.innerHTML = ''
+
+  const mealIds = getMealsFromLocalStorage()
+
+  for (const mealId of mealIds) {
+    const meal = await getMealById(mealId)
+
+    addMealToFavorites(meal)
+  }
+}
+
+function addMealToFavorites(mealData) {
+  const favoriteMeal = document.createElement('li')
+
+  favoriteMeal.innerHTML = `
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        /><span>${mealData.strMeal}</span>
+        <button class="clear"><i class="fas fa-window-close"></i></button>
+    `
+
+  const btn = favoriteMeal.querySelector('.clear')
+
+  btn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    removeMealFromLocalStorage(mealData.idMeal)
+
+    fetchFavorites()
+  })
+
+  favoriteMeal.addEventListener('click', () => {
+    showMealInfo(mealData)
+  })
+
+  favoriteContainer.appendChild(favoriteMeal)
+}
+
+function showMealInfo(mealData) {
+  // clean it up
+  mealInfoElement.innerHTML = ''
+
+  // update the Meal info
+  const mealElement = document.createElement('div')
+
+  const ingredients = []
+
+  // get ingredients and measures
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = mealData[`strIngredient${i}`]
+    const measure = mealData[`strMeasure${i}`]
+
+    if (!ingredient) {
+      break
+    }
+
+    ingredients.push(`${ingredient} - ${measure}`)
+  }
+
+  mealElement.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+        </ul>
+    `
+
+  mealInfoElement.appendChild(mealElement)
+
+  // show the popup
+  mealPopup.classList.remove('hidden')
+}
+
+searchBtn.addEventListener('click', async () => {
+  // clean container
+  mealsElement.innerHTML = ''
+
+  const search = searchTerm.value
+  const meals = await getMealsBySearch(search)
+
+  if (meals) {
+    meals.forEach((meal) => {
+      addMeal(meal)
+    })
+  }
+})
+
+popupCloseBtn.addEventListener('click', () => {
+  mealPopup.classList.add('hidden')
+})
